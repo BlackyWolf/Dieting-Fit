d4c0b93712f52fcc93cbb98f6530734a0bcb6873
Blacky Wolf <support@blackywolf.me>
2022-03-20 22:48:44 -0500

    Added resizing support for logo

--- app/app/ui/branding/Logo.tsx
+++ app/app/ui/branding/Logo.tsx
@@ -1,9 +1,34 @@
 import { faUserChef } from '@fortawesome/pro-duotone-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { joinClasses } from '~/utilities';
 
-export const Logo = () => {
+type LogoSize = 'sm' | 'md' | 'lg';
+
+interface Properties {
+    size?: LogoSize
+}
+
+function getSizeClass(size: LogoSize) {
+    switch (size) {
+        case 'sm':
+            return 'sm:text-2xl md:text-3xl lg:text-4xl';
+
+        case 'lg':
+            return '';
+
+        default:
+            return 'text-5xl';
+    }
+}
+
+export const Logo = ({ size = 'md' }: Properties) => {
+    const classes = joinClasses(
+        'font-nunito font-black uppercase text-neutral-500',
+        getSizeClass(size)
+    );
+
     return (
-        <div className="font-nunito font-black uppercase text-5xl text-neutral-500">
+        <div className={classes}>
             <span>DIETING</span>
 
             <FontAwesomeIcon icon={faUserChef} className="text-lime-500 mx-1" />
