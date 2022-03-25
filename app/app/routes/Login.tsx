16961b15eb79639c25bcdcbdfc80e61a23d009e8
Blacky Wolf <support@blackywolf.me>
2022-03-24 20:12:21 -0500

    Updated login background

--- app/app/routes/Login.tsx
+++ app/app/routes/Login.tsx
@@ -1,5 +1,7 @@
 import { Link } from 'remix';
 import { Logo } from '~/ui';
+// Photo by Trang Doan from Pexels: https://www.pexels.com/photo/assorted-sliced-fruits-1128678/
+import backgroundImage from '~/images/pexels-trang-doan-1128678.jpg';
 
 export default function Login() {
     return (
@@ -151,8 +153,8 @@ export default function Login() {
             <div className="hidden lg:block relative w-0 flex-1">
                 <img
                     className="absolute inset-0 h-full w-full object-cover"
-                    src="https://images.unsplash.com/photo-1505904267569-f02eaeb45a4c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1908&q=80"
-                    alt=""
+                    src={backgroundImage}
+                    alt="Login Background, original photo taken by Trang Doan"
                 />
             </div>
         </div>
