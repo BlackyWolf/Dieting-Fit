884c1d7cf1eac1740817de8db4f14d88d4a8f699
Blacky Wolf <support@blackywolf.me>
2022-05-21 17:32:00 -0500

    Fixed up UI and error messages

--- app/app/routes/Signin.tsx
+++ app/app/routes/Signin.tsx
@@ -170,7 +170,7 @@ export default function Login() {
                                                 id="remember-me"
                                                 name="remember-me"
                                                 type="checkbox"
-                                                className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+                                                className="h-4 w-4 text-blue-500 focus:ring-blue-400 border-gray-300 rounded"
                                                 value="true"
                                             />
                                             <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
