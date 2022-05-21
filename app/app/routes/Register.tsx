327a4ce46c3d29cb42efaf8087d0d681496e3607
Blacky Wolf <support@blackywolf.me>
2022-05-21 17:31:39 -0500

    Made it possible to use email for username

--- app/app/routes/Register.tsx
+++ app/app/routes/Register.tsx
@@ -1,4 +1,5 @@
 import { Form, Link, redirect, useActionData, useTransition } from 'remix';
+import { useState } from 'react';
 import type { ActionFunction } from 'remix';
 import { badRequest, internalServerError } from '~/data/responses.server';
 import { buildCreateUserData, createUserSession, registerUserAsync, validateRegisterUserFormAsync } from '~/data/user';
@@ -6,6 +7,7 @@ import { Logo, PrimaryButton } from '~/ui';
 
 // Photo by Burst from Pexels
 import backgroundImage from '~/images/pexels-burst-374052.jpg';
+import { joinClasses } from '~/utilities';
 
 export const action: ActionFunction = async ({ request }) => {
     const form = await request.formData();
@@ -30,10 +32,30 @@ export const action: ActionFunction = async ({ request }) => {
 };
 
 export default function Login() {
+    const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
+    const [useEmailAsUsername, setUseEmailAsUsername] = useState(false);
     const transition = useTransition();
     const action = useActionData();
 
     const submitting = transition.state === 'submitting';
+
+    function toggleEmailAsUsername() {
+        const newStatus = !useEmailAsUsername;
+        setUseEmailAsUsername(newStatus);
+
+        setUsername(newStatus ? email : '');
+    }
+
+    function updateEmail(event: any) {
+        setEmail(event.target.value);
+
+        if (useEmailAsUsername) setUsername(event.target.value);
+    }
+
+    function updateUsername(event: any) {
+        setUsername(event.target.value);
+    }
 
     return (
         <div className="min-h-full flex flex-grow">
@@ -66,15 +88,23 @@ export default function Login() {
                                         <label htmlFor="username" className="block text-sm font-medium text-gray-700">
                                             Username
                                         </label>
+
                                         <div className="mt-1">
                                             <input
                                                 id="username"
                                                 name="username"
                                                 type="username"
                                                 required
-                                                className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                                readOnly={useEmailAsUsername}
+                                                value={useEmailAsUsername ? email : username}
+                                                onInput={updateUsername}
+                                                className={joinClasses(
+                                                    'appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm',
+                                                    useEmailAsUsername ? 'bg-gray-200' : ''
+                                                )}
                                             />
                                         </div>
+
                                         <p><small className="text-red-500 font-bold">{action?.fieldErrors?.username}</small></p>
                                     </div>
 
@@ -82,22 +112,42 @@ export default function Login() {
                                         <label htmlFor="email" className="block text-sm font-medium text-gray-700">
                                             Email address
                                         </label>
+
                                         <div className="mt-1">
                                             <input
                                                 id="email"
                                                 name="email"
                                                 type="email"
                                                 required
+                                                onInput={updateEmail}
                                                 className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                                             />
                                         </div>
+
                                         <p><small className="text-red-500 font-bold">{action?.fieldErrors?.email}</small></p>
+                                    </div>
+
+                                    <div className="flex items-center">
+                                        <input
+                                            id="email-as-username"
+                                            name="email-as-username"
+                                            type="checkbox"
+                                            className="h-4 w-4 text-blue-500 focus:ring-blue-400 border-gray-300 rounded"
+                                            value="true"
+                                            checked={useEmailAsUsername}
+                                            onChange={toggleEmailAsUsername}
+                                        />
+
+                                        <label htmlFor="email-as-username" className="ml-2 block text-sm text-gray-900">
+                                            Use email as username
+                                        </label>
                                     </div>
 
                                     <div className="space-y-1">
                                         <label htmlFor="password" className="block text-sm font-medium text-gray-700">
                                             Password
                                         </label>
+
                                         <div className="mt-1">
                                             <input
                                                 id="password"
@@ -107,6 +157,7 @@ export default function Login() {
                                                 className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                                             />
                                         </div>
+
                                         <p><small className="text-red-500 font-bold">{action?.fieldErrors?.password}</small></p>
                                     </div>
 
