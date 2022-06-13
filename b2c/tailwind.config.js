7ce0c6c1dc5f212b1c5f496581838e37dfe8bfce
Blacky Wolf <support@blackywolf.me>
2022-06-12 22:32:38 -0500

    Added CSS for registration and email verification

--- b2c/tailwind.config.js
+++ b2c/tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        './*.html'
+        './*.html',
+        './pages/*.html'
     ],
     plugins: [],
     theme: {
