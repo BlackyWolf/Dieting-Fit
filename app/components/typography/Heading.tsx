6213e8129ec0eeb90102d86841ed382d891c8a4b
Blacky Wolf <support@blackywolf.me>
2022-07-05 00:00:52 -0500

    Decreased heading size by one

--- app/components/typography/Heading.tsx
+++ app/components/typography/Heading.tsx
@@ -12,11 +12,11 @@ interface Properties {
 
 export const Heading = ({ children, size }: Properties) => {
     switch (size) {
-        case "1": return <h1 class={tw`font-semibold text-6xl`}>{children}</h1>;
-        case "2": return <h2 class={tw`font-semibold text-5xl`}>{children}</h2>;
-        case "3": return <h3 class={tw`font-semibold text-4xl`}>{children}</h3>;
-        case "4": return <h4 class={tw`font-semibold text-3xl`}>{children}</h4>;
-        case "5": return <h5 class={tw`font-semibold text-2xl`}>{children}</h5>;
+        case "1": return <h1 class={tw`font-semibold text-5xl`}>{children}</h1>;
+        case "2": return <h2 class={tw`font-semibold text-4xl`}>{children}</h2>;
+        case "3": return <h3 class={tw`font-semibold text-3xl`}>{children}</h3>;
+        case "4": return <h4 class={tw`font-semibold text-2xl`}>{children}</h4>;
+        case "5": return <h5 class={tw`font-semibold text-xl`}>{children}</h5>;
         case "6": return <h6 class={tw`font-semibold text-lg`}>{children}</h6>;
         default: return null;
     }
