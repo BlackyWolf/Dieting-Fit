058ea2c98d5256c1c76ecfcf4f96bd401e8e837e
Blacky Wolf <support@blackywolf.me>
2022-07-27 20:21:57 -0500

    Added msal to index for auth folder

--- app/frontend/src/main.tsx
+++ app/frontend/src/main.tsx
@@ -1,9 +1,9 @@
 import { MsalProvider } from '@azure/msal-react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { msal } from './auth';
 import { App } from './App';
 import './assets/css/main.css';
-import { msal } from './auth/msal';
 
 const root = createRoot(
     document.getElementById('root') as HTMLElement
