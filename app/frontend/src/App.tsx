23fe5e956df489bd02131c42d71c2513f6e23da8
Blacky Wolf <support@blackywolf.me>
2022-07-27 20:21:36 -0500

    Added logout and debugging

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -1,19 +1,25 @@
 import { AuthenticatedTemplate, UnauthenticatedTemplate, useMsal } from '@azure/msal-react';
 import { BrowserRouter } from 'react-router-dom';
+import { scopes } from './auth';
 import { AppRoutes } from './routing';
 
 export const App = () => {
-    const { instance } = useMsal();
+    const { accounts, instance } = useMsal();
+
+    if (accounts.length > 0) {
+        console.log(accounts[0]);
+    }
 
     return (
         <>
             <AuthenticatedTemplate>
+                <button className="bg-slate-500 text-white px-4 py-2 rounded-md" onClick={() => instance.logoutRedirect()}>Logout</button>
                 <BrowserRouter>
                     <AppRoutes />
                 </BrowserRouter>
             </AuthenticatedTemplate>
             <UnauthenticatedTemplate>
-                <button onClick={() => instance.loginRedirect()}>Login</button>
+                <button className="bg-blue-500 text-white px-4 py-2 rounded-md" onClick={() => instance.loginRedirect({ scopes })}>Login</button>
             </UnauthenticatedTemplate>
         </>
     );
